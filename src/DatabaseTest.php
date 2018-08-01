92af008c04e7830b7b9db9cb6b605b375c773545
febry <febririzki46@yahoo.com>
2018-08-01 08:09:13 +0700

    2

---
+++ src/DatabaseTest.php
@@ -0,0 +1,30 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    
+    public static function GetConnection()
+    {
+        $user     = 'root';
+        $password = '';
+        $pdo      = new PDO('mysql:dbname=testdb;host=127.0.0.1', $user, $password);
+        
+        return $pdo;
+    }
+    
+    public static function testCreateTable()
+    {
+        Self::getConnection()->query("CREATE TABLE belajar (hasil VARCHAR(50) NOT NULL)");
+    }
+    
+    public static function testInsertTable()
+    {
+        $sql = "INSERT INTO belajar (hasil) VALUES ('John')";
+        // use exec() because no results are returned
+        Self::getConnection()->exec($sql);
+    }
+    
+}
+
+?>
