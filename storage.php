5ec98cc29dc343836a5f83d5977a5c530adb1a61
febririzki461 <febry.rizky@cbncloud.co.id>
2018-08-15 10:38:56 +0700

    Delete storage.php
    remove

--- storage.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-<?php
-$image = "https://venturebeat.com/wp-content/uploads/2017/12/microsoft-azure-new-logo-2017.png?fit=578%2C334&strip=all";
-?>
-
-<img src =<?php echo $image;?> ></img>
